0c5f9f8c0537086f7023100d89c55583fcb12960
dnyaneshwarmagar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2022-04-10 21:51:14 +0530

    mobileOs

---
+++ mobileOs/src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const mobileOs = ["Android", "Blackberry", "Iphone", "Windows Phone"]
+  let manufacture = ["Samsung", "HTC", "Micromax", "Apple"]
+  return (
+    <div className="App">
+      <h1>Mobile Operating System</h1>
+      <ul>
+        {mobileOs.map(el => {
+          return <li>{el}</li>
+        })}
+      </ul>
+      <h1>Mobile Manufacture</h1>
+      <ul>
+        {manufacture.map(el => {
+          return <li>{el}</li>
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
