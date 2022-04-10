89d2baf2b21d9a4e461203fed117e036964d9065
dnyaneshwarmagar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2022-04-10 11:29:31 +0530

    ver

--- trialVercel/webpack.config.js
+++ trialVercel/webpack.config.js
@@ -2,10 +2,7 @@ let path = require("path");
 
 module.exports = {
     entry: "./src/index.js",
-    output: {
-        path: path.resolve(".", "build"),
-        filename: "bundle.js"
-    },
+   
     mode: "development",
     module: {
         rules: [
